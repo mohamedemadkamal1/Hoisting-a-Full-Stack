c0c2149a0bbf2cfbd6bd5126c640ed7775917507
mohamedemad <mohamedemadkamal1@gmail.com>
2022-07-02 23:08:49 +0200

    change

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=password123
+export POSTGRES_HOST=database-1.cpgcddggrbdl.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mehostingabucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
+export JWT_SECRET=igni123
 export URL=http://localhost:8100
